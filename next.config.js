98cb8697cfa7607619196055a6c332765d9d16b0
Reirysson <reiryssonjp@gmail.com>
2022-02-05 20:48:58 -0300

    mudança no next

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 const config = {
     webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        issuer: {
-          and: [/\.(js|ts)x?$/]
-        },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              prettier: false,
-              svgo: true,
-              svgoConfig: { plugins: [{ removeViewBox: false }] },
-              titleProp: true,
-            },
-          },
-        ],
-      });
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+      })
 
       return config
 },
